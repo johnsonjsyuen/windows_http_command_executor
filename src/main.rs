e511bb003af952006428b81358e5d6545ab53eaf
johnson yuen <j@johnsonyuen.com>
2022-07-17 16:04:00 +1200

    Add nice templating instead of raw string for index. Update crates

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 extern crate rocket;
 extern crate system_shutdown;
 
+use rocket::{Build, Rocket};
+use rocket_dyn_templates::{context, Template};
 use system_shutdown::{reboot, shutdown};
 
 #[get("/shutdown")]
-fn shutdown_handler() -> std::string::String {
+fn shutdown_handler() -> String {
     match shutdown() {
         Ok(_) => "Shutting down, bye!".into(),
         Err(error) => format!("Failed to shut down: {}", error),
@@ -13,7 +15,7 @@ fn shutdown_handler() -> std::string::String {
 }
 
 #[get("/reboot")]
-fn reboot_handler() -> std::string::String {
+fn reboot_handler() -> String {
     match reboot() {
         Ok(_) => "Rebooting, bye!".into(),
         Err(error) => format!("Failed to reboot: {}", error),
@@ -21,14 +23,15 @@ fn reboot_handler() -> std::string::String {
 }
 
 #[get("/")]
-fn index_handler() -> &'static str {
-    "Available options: /shutdown /reboot"
+fn index_handler() -> Template {
+    Template::render("index", context! {})
 }
 
 #[launch]
-fn rocket() -> _ {
+fn rocket() -> Rocket<Build> {
     rocket::build()
         .mount("/", routes![shutdown_handler])
         .mount("/", routes![reboot_handler])
         .mount("/", routes![index_handler])
+        .attach(Template::fairing())
 }
